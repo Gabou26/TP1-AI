1b3860dbc552910ebcf939e49f90650e865fa924
Gabou26 <gabriel262626@hotmail.com>
2022-02-14 15:28:42 -0500

    Updates uninformed code so it's cleaner and works better

--- exploration/uninformed.py
+++ exploration/uninformed.py
@@ -10,50 +10,62 @@ Légende: Actions Robot
 
 
 class Uninformed:
+    path = []
+    pos_cour_x = 0
+    pos_cour_y = 0
+    diff_x = 0
+    diff_y = 0
+
     def __call__(self) -> None:
         return
 
     #Params : Pos Robot - Pos Destination
     def calculate_path(self, robot_x, robot_y, dest_x, dest_y, matrix) -> []:
-        pos_cour_x = robot_x
-        pos_cour_y = robot_y
-        path = []
+        self.pos_cour_x = robot_x
+        self.pos_cour_y = robot_y
+        self.path = []
 
         #Calcul Différentiel
-        diff_x = dest_x - robot_x
-        diff_y = dest_y - robot_y
+        self.diff_x = dest_x - robot_x
+        self.diff_y = dest_y - robot_y
 
-        # Mouv X
-        while diff_x != 0:
-            if diff_x > 0:
-                pos_cour_x += 1
-                diff_x -= 1
-                path.append(1)
-            elif diff_x < 0:
-                pos_cour_x -= 1
-                diff_x += 1
-                path.append(0)
-            if self.contains_dust(pos_cour_x, pos_cour_y, matrix):
-                path.append(4)
+        # Mouv X + Action
+        while self.diff_x != 0:
+            if self.diff_x > 0:
+                self.move(1, 0, matrix)
+            elif self.diff_x < 0:
+                self.move(-1, 0, matrix)
 
-        # Mouv Y
-        while diff_y != 0:
-            if diff_y > 0:
-                pos_cour_y += 1
-                diff_y -= 1
-                path.append(2)
-            elif diff_y < 0:
-                pos_cour_y -= 1
-                diff_y += 1
-                path.append(3)
+        # Mouv Y + Action
+        while self.diff_y != 0:
+            if self.diff_y > 0:
+                self.move(0, 1, matrix)
+            elif self.diff_y < 0:
+                self.move(0, -1, matrix)
 
-            #Check Actions
-            if self.contains_dust(pos_cour_x, pos_cour_y, matrix):
-                path.append(4)
-            elif self.contains_jewel(pos_cour_x, pos_cour_y, matrix):
-                path.append(5)
+        #Retourne chemin d'actions complet
+        return self.path
 
-        return path
+    def move(self, move_x, move_y, matrix):
+        #Mouvement
+        self.pos_cour_x += move_x
+        self.pos_cour_y += move_y
+        self.diff_x -= move_x
+        self.diff_y -= move_y
+        if move_x < 0:
+            self.path.append(0)
+        if move_x > 0:
+            self.path.append(1)
+        if move_y > 0:
+            self.path.append(2)
+        if move_y < 0:
+            self.path.append(3)
+
+        # Check Actions
+        if self.contains_dust(self.pos_cour_x, self.pos_cour_y, matrix):
+            self.path.append(4)
+        elif self.contains_jewel(self.pos_cour_x, self.pos_cour_y, matrix):
+            self.path.append(5)
 
     def contains_dust(self, pos_x, pos_y, matrix):
         data_id = matrix[pos_x, pos_y]
